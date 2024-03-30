e8c9b038144a17957354b1e2489db8563c390b1d
HEEE23 <gmltjs1592@gmail.com>
2024-03-31 01:00:38 +0900

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+// 자바는 객체지향프로그래밍이기 때문에 클래스단위로 코딩을 한다.
+// 시간, 메일 클래스
+// JavaSE 프로그램
+public class Calculator {
+    // main() {   } 메서드
+    public static void main(String[] args){
+        // Q. 두 개의 정수를 더하여 출력하는 자바 프로그램을 만들어 보자
+        int a, b, sum;
+        a = 1  ;
+        b = 1;
+        sum = a + b;
+
+        // 출력 sout
+        System.out.println(sum); // 2
+    }
+}
---
+++ src/FindAPI.java
@@ -0,0 +1,13 @@
+public class FindAPI {
+    public static void main(String[] args){
+        int a; // 변수선언
+        a = 10;
+        System.out.println("a = " + a);
+        // "APPLE" 문자열 저장하고 출력
+        java.lang.String s;
+        s = "APPLE";
+        System.out.println("s = " + s);
+        System.out.println(s.length());
+    }
+}
+  
