135246a344b01ff6a1ecbc704f0e2494727adbd5
HEEE23 <gmltjs1592@gmail.com>
2024-04-22 23:11:37 +0900

    배열

---
+++ src/ArrayMake.java
@@ -0,0 +1,10 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        // Q. [정수 5개]를 저장 할 [배열을 생성]하시요.
+        int[] a = new int[5];
+        float[] f = new float[5]; // 길이가 고정
+
+        Book b1, b2, b3;
+        Book[] b = new Book[3]; // 책 3권을 저장할 배열을 생성
+    }
+}
